c89706c5b3956b8632482a64e9db7d8a3bb62097
Starry North <805408477@qq.com>
2022-06-28 10:51:27 +0800

    后端完成

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-from paddlenlp import Taskflow
+from fastapi import FastAPI
+from routers.uie import mod as uie
 
+app = FastAPI()
 
-def main():
-    schema = ['时间', '选手', '赛事名称'] # Define the schema for entity extraction
-    ie = Taskflow('information_extraction', schema=schema)
-    print(ie("2月8日上午北京冬奥会自由式滑雪女子大跳台决赛中中国选手谷爱凌以188.25分获得金牌！")) # Better print results using pprint
-
-if __name__ == "__main__":
-    main()
+app.include_router(uie.router)
